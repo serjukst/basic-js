6165c4541f73a150cfa4284676a7319ae15eabbe
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2020-02-25 22:09:56 +0200

    feat: add solution for transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,33 @@
 module.exports = function transform(arr) {
     if (arr.length === 0) return [];
     if (!Array.isArray(arr)) throw new Error();
+    
+    let newArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == '--double-next') {
+            
+            if(i + 1 < arr.length) {
+                newArr.push(arr[i + 1]);  
+            }
+            
+        } else if (arr[i] == '--double-prev') {
+
+            if (i - 1 >= 0) {
+                newArr.push(arr[i - 1]);
+            }
+            
+        } else if (arr[i] == '--discard-next') {
+            i++;
+            
+        } else if (arr[i] == '--discard-prev') {
+            newArr.pop()
+            
+        } else {
+            newArr.push(arr[i]);
+        }
+    }
+    
+    return newArr;
 };
+
