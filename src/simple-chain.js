54bb81627aa10216f04da171de50b29c791d5d3a
Serhii Stepanchuk <51526907+serjukst@users.noreply.github.com>
2020-03-21 21:18:05 +0200

    feat: add solution for simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,18 +5,27 @@ const chainMaker = {
     return this.chain.length;
   },
   addLink(value) {
-    this.chain.push(value);
+    this.chain.push(`( ${value} )`);
     return this;
   },
   removeLink(position) {
-    
+    if ( position <= 0 || position >= this.chain.length || typeof position !== "number" || !Number.isInteger(position) ) {
+      this.chain = [];
+      throw Error;
+    }
+
+    this.chain.splice(position-1, 1);
+    return this;
   },
   reverseChain() {
     this.chain.reverse();
     return this;
   },
   finishChain() {
-    
+    const resultChain = this.chain.slice();
+    this.chain = [];
+
+    return resultChain.join('~~');
   }
 };
 
